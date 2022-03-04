214d7eda0873f04293a9eb1b365ad0f7496c5ece
alirezajs <alireza.varmgahni@gmail.com>
2022-03-04 11:46:28 +0300

    Add time to the game

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -4,23 +4,69 @@ import RandomNumber from "./RandomNumber";
 
 import React from "react";
 
-interface gameProps {
+interface GameProps {
   randomNumberCount: number;
+  remindingSecond: number;
 }
 
-interface gameInterface {
+interface GameState {
   selectedIds: number[];
+  remindingSecond: number;
 }
-
-class Game extends React.Component<gameProps, gameInterface> {
-  constructor(props: { randomNumberCount: number }) {
+interface GameInterface {
+  intervalId: any;
+  gameStatus: "win" | "lost" | "playing";
+  target: number;
+  isIdSelected: (numberIndex: number) => boolean;
+  selectId: (numberIndex: number) => void;
+  CalculateGameStatus: (state: GameState) => "win" | "lost" | "playing";
+}
+class Game
+  extends React.Component<GameProps, GameState>
+  implements GameInterface
+{
+  constructor(props: GameProps) {
     super(props);
 
     this.state = {
       selectedIds: [],
+      remindingSecond: this.props.remindingSecond,
     };
 
     this.selectId = this.selectId.bind(this);
+  }
+
+  intervalId: any;
+  gameStatus: "playing" | "win" | "lost" = "playing";
+
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.setState(
+        (prevState) => {
+          return { remindingSecond: prevState.remindingSecond - 1 };
+        },
+        () => {
+          if (this.state.remindingSecond === 0) {
+            clearInterval(this.intervalId);
+          }
+        }
+      );
+    }, 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  componentWillUpdate(nextProps: GameProps, nextState: GameState) {
+    if (
+      nextState.selectedIds !== this.state.selectedIds ||
+      nextState.remindingSecond === 0
+    ) {
+      this.gameStatus = this.CalculateGameStatus(nextState);
+      if (this.gameStatus != "playing") {
+        clearInterval(this.intervalId);
+      }
+    }
   }
 
   randomNumbers = Array.from({ length: this.props.randomNumberCount }).map(
@@ -40,20 +86,22 @@ class Game extends React.Component<gameProps, gameInterface> {
     }));
   }
 
-  getStatus = (): "win" | "lost" | "playing" => {
-    const sumSelected = this.state.selectedIds.reduce((acc, curr) => {
+  CalculateGameStatus = (nextState: GameState): "win" | "lost" | "playing" => {
+    const sumSelected = nextState.selectedIds.reduce((acc, curr) => {
       return acc + this.randomNumbers[curr];
     }, 0);
+    if (nextState.remindingSecond === 0) return "lost";
     if (sumSelected === this.target) return "win";
     if (sumSelected > this.target) return "lost";
     return "playing";
   };
+
   render() {
-    const status = this.getStatus();
     return (
       <View style={styles.container}>
-        <Text>{status}</Text>
-        <Text style={[styles.target, styles[`status_${status}`]]}>
+        <Text>{this.gameStatus}</Text>
+        <Text>{this.state.remindingSecond}</Text>
+        <Text style={[styles.target, styles[`status_${this.gameStatus}`]]}>
           {this.target}
         </Text>
         <View style={styles.randomContainer}>
@@ -62,7 +110,9 @@ class Game extends React.Component<gameProps, gameInterface> {
               key={index}
               id={index}
               randomNumber={randomNumber}
-              isDisabled={this.isIdSelected(index) || status !== "playing"}
+              isDisabled={
+                this.isIdSelected(index) || this.gameStatus !== "playing"
+              }
               onPress={this.selectId}
             />
           ))}
